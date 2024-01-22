e5fa35029b52327440d9779e77026c3a0b36fbff
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-01-22 14:17:30 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,24 +4,30 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <div>
-
-<h1>Ruxandra Serbanoiu</h1>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/sketchbook">Sketchbook</Link>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/chocolatechiphelmet/ " target='blank'>Instagram</a>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+      <Link to="/">
+        <h1>Ruxandra Serbanoiu</h1>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/sketchbook">Sketchbook</Link>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/chocolatechiphelmet/ "
+              target="blank"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
