9f0c103d641508ab098564d6a4c19d21719407f3
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-01-29 13:19:24 +0200

    lucru la contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,40 +1,51 @@
+import Navigation from './Navigation';
+
 export default function Contact() {
   return (
     <div>
-      <div>
-        <h2>Get in touch:</h2>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="">Message:</label>
-        <input
-          type="text"
-          id="message"
-          name="message"
-        />
-        <input
-          type="submit"
-          value="Submit"
-        />
-      </form>
-      <p>or send email @</p>
-      <p>chocolatechiphelmet@gmail.com</p>
-      </div>
-      <div>
-      <img
-        src="https://placehold.co/600x400"
-        alt=""
-      />
+      <Navigation />
+      <div className="containerContact">
+        <div>
+          <h2>Get in touch:</h2>
+          <form>
+            <div className='inputDiv'>
+              <label htmlFor="name"></label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name:"
+              />
+              <label htmlFor="email"></label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email:"
+              />
+              <label htmlFor=""></label>
+              <input
+                type="text"
+                id="message"
+                name="message"
+                placeholder="Message:"
+              />
+              <input
+                type="submit"
+                value="Submit"
+                id="submit"
+              />
+            </div>
+          </form>
+          <p>or send email @</p>
+          <p>chocolatechiphelmet@gmail.com</p>
+        </div>
+        <div className="containerPozaContact">
+          <img
+            src="/portfolio/field.jpg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
